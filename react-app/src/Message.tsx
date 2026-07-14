f58b1579548886eac830564a99afc985805be335
Hariom Pandya <pandya.hariom@gmail.com>
2026-07-14 16:58:13 +0530

    1. Using JSX inject javascript expression in html to create dynamic content

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,5 +1,12 @@
 
 function Message(){
-    return <h1> Hello React</h1>
+    const name = "Hariom";
+    if (name) {
+        // Using JSX: Javascript XML
+        return <h1> Hello {name}</h1>
+    }
+    else {  
+        return <h1> Hello React</h1>
+    }
 }
 export default Message;
