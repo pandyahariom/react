50d3f19f3ffa0c5f142fa9bf61a0ded4e8fe292d
Hariom Pandya <pandya.hariom@gmail.com>
2026-07-15 16:25:59 +0530

    9. Using arrow function in onClick event handler to pass additional arguments (item and index) to the handleClickEvent function. This allows us to log the item and index along with the event object when an item is clicked.

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -3,12 +3,15 @@ function ListGroup() {
   //  Reference of Bootstrap ListGroup:  https://getbootstrap.com/docs/5.3/components/list-group/
   let items = ["JavaScript", "React", "TypeScript", "MongoDB", "Angular"];
 
-  const handleClickEvent = (event: MouseEvent) => {
+  const handleClickEvent = (event: MouseEvent, item: string, index: number) => {
+    // Here typeScript require us to specify the type of all arguments.
+    // For the event argument, we can use the MouseEvent type from React (as we have imported above)
+
     // onClick expect a callback function which will be called when the event is triggered.
     // The callback function will receive the event object as an argument.
     // The event object contains information about the event that occurred,
     // such as the type of event, the target element etc.
-    console.log(event);
+    console.log(index + " : " + item + " : " + event);
   };
   return (
     // Any react component can return only one element,
@@ -34,8 +37,12 @@ function ListGroup() {
       {items.length === 0 && <p>No items found !</p>}
 
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item" onClick={handleClickEvent}>
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className="list-group-item"
+            onClick={(event) => handleClickEvent(event, item, index)}
+          >
             {item}
           </li>
         ))}
