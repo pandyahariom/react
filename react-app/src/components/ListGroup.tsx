f9b5fd8bdaec6233bb3f5431c25acb9605399f0b
Hariom Pandya <pandya.hariom@gmail.com>
2026-07-15 16:21:57 +0530

    8. Passing a callback to onClick to handle the click event.# Please enter the commit message for your changes. Lines starting

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,16 +1,25 @@
+import { MouseEvent } from "react";
 function ListGroup() {
-    //  Reference of Bootstrap ListGroup:  https://getbootstrap.com/docs/5.3/components/list-group/
-    let items = ['JavaScript', 'React', 'TypeScript', 'MongoDB', 'Angular'];
-    return (
-        // Any react component can return only one element, 
-        // so we can use Fragment to wrap multiple elements.
-        // We can use <> and </> or <Fragment> and </Fragment> to wrap multiple elements.
-        // To use Fragment, we need to import it from React as import {Fragment} from "react";
-        
-        <>
-            <h3>List Group</h3>
+  //  Reference of Bootstrap ListGroup:  https://getbootstrap.com/docs/5.3/components/list-group/
+  let items = ["JavaScript", "React", "TypeScript", "MongoDB", "Angular"];
 
-            {/* 
+  const handleClickEvent = (event: MouseEvent) => {
+    // onClick expect a callback function which will be called when the event is triggered.
+    // The callback function will receive the event object as an argument.
+    // The event object contains information about the event that occurred,
+    // such as the type of event, the target element etc.
+    console.log(event);
+  };
+  return (
+    // Any react component can return only one element,
+    // so we can use Fragment to wrap multiple elements.
+    // We can use <> and </> or <Fragment> and </Fragment> to wrap multiple elements.
+    // To use Fragment, we need to import it from React as import {Fragment} from "react";
+
+    <>
+      <h3>List Group</h3>
+
+      {/* 
              Use of conditional rending: If the items array is empty, we can show a message to the user.
              Way 1: Using Ternary Operator 
                     Ex. {items.length === 0 ? <p>No items found !</p> : null }
@@ -20,16 +29,19 @@ function ListGroup() {
                     Ex. Outside return write function renderMessage() { return items.length === 0 ? <p>No items found !</p> : null; } and then use {renderMessage()} in the JSX.
              Way 4: We can use && operator to conditionally render the message. 
                     Ex. This is used in next line.
-             */
-            }
-            
-            {items.length === 0 && <p>No items found !</p> }
-            
-            <ul className="list-group">
-                {items.map(item => <li key={item} className="list-group-item">{item}</li>)}
-            </ul>
-        </>
-    );
+             */}
+
+      {items.length === 0 && <p>No items found !</p>}
+
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item} className="list-group-item" onClick={handleClickEvent}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
