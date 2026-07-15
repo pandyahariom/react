9c031259395957c488f29292b146c700d245ff0d
Hariom Pandya <pandya.hariom@gmail.com>
2026-07-15 11:50:47 +0530

    5. use <> to let react return multiple elements without adding extra DOM element

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -2,9 +2,17 @@ function ListGroup() {
     //  Reference of Bootstrap ListGroup:  https://getbootstrap.com/docs/5.3/components/list-group/
     let items = ['JavaScript', 'React', 'TypeScript', 'MongoDB', 'Angular'];
     return (
-        <ul className="list-group">
-            {items.map(item => <li className="list-group-item">{item}</li>)}
-        </ul>
+        // Any react component can return only one element, 
+        // so we can use Fragment to wrap multiple elements.
+        // We can use <> and </> or <Fragment> and </Fragment> to wrap multiple elements.
+        // To use Fragment, we need to import it from React as import {Fragment} from "react";
+        
+        <>
+            <h3>List Group</h3>
+            <ul className="list-group">
+                {items.map(item => <li className="list-group-item">{item}</li>)}
+            </ul>
+        </>
     );
 }
 
